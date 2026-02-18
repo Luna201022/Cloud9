c8ed3ef4ba1a6f6fe6c8cf7ef41a26f5c4079631
Luna201022 <thomashollandmoritz313@gmail.com>
2026-02-18 18:19:47 +0100

    Add files via upload

--- functions/menu.de.json.js
+++ functions/menu.de.json.js
@@ -1,15 +1,23 @@
-export async function onRequestGet({ env }) {
+export async function onRequestGet({ request, env, next }) {
+  // 1) Erst KV versuchen
   const key = "menu.de.json";
   const data = await env.MENU_KV.get(key);
 
-  if (!data) {
-    return new Response(
-      JSON.stringify({ error: "Menu not set yet" }),
-      { status: 404, headers: { "content-type": "application/json; charset=utf-8" } }
-    );
+  if (data) {
+    return new Response(data, {
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+        "cache-control": "no-store"
+      }
+    });
   }
 
-  return new Response(data, {
+  // 2) Fallback: originale Datei aus dem Repo (Pages Asset) ausliefern
+  // next() liefert die statische Datei /menu.de.json aus deinem GitHub-Deploy aus
+  const resp = await next();
+  // sicherstellen, dass es JSON bleibt
+  return new Response(resp.body, {
+    status: resp.status,
     headers: {
       "content-type": "application/json; charset=utf-8",
       "cache-control": "no-store"
